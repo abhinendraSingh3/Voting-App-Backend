dcb4da5f4846ab339b0921e509bca7b90dd43eef
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2026-01-12 21:18:04 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,54 @@
-const express= require('express');
-const router= express.Router();
-const userSchema=require('./../models/user')
+const express = require('express');
+const router = express.Router();
+const userSchema = require('./../models/user')
+const bcrypt = require('bcrypt');
 
 //post method to add login credentials
-router.post('/signup',async(req,res)=>{
+router.post('/signup', async (req, res) => {
 
-const userData=req.body;// assuming the requestbody contains the user data
+    try {
+        const { name, address, aadharNumber, password, role } = req.body;// assuming the requestbody contains the user data
 
-//creating new user document using the mongoose model
-const createU=new userSchema(userData);
+        //check if the required field has been set or not
+        if (!name || address || aadharNumber || password || role) {
+            return res.status(400).json({
+                message: "All fields are required"
+
+            })
+        }
+
+        //checking if the user already existed or not
+        const checkUser = await userSchema.findOne({ aadharNumber })
+        if (checkUser) {
+            return res.status(400).json({
+                message: "User already existed"
+            });
+        }
+
+        //storing password using bcrypt
+        const saltRounds = 10;
+        //hashpass
+        const hassPass = await bcrypt.hash(password, saltRounds);
+
+        //creating new user document using the mongoose model
+        const createU = new userSchema({
+            ...req.body, //takes all the field automaticall as ... is a spreadOperator.
+            password: hassPass // overRides plain password
+        });
+
+        // save the data to db
+        const response = await createU.save();
+        console.log("data saved in db");
+        res.status(201).json({
+            message: 'User Registered Successfuly'
+        })
+
+        //if something broke then.
 
 
+    }
+    catch (error) {
+        res.status(500).json({ error: "Internal Server Error" })
 
-
+    }
 })
