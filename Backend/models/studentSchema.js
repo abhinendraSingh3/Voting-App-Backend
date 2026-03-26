f41ac8ba32f3d320a9dbce039e24949d2f653ac4
abhinendraSingh3 <abhinendrayadav3@gmail.com>
2026-03-26 16:00:12 +0530

    added schemas

---
+++ Backend/models/studentSchema.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+//define user schema
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: [3, 'Name must be atleast 3 characters long'],
+        maxLength: [50, 'Name cannot exceed 50 characters']
+    },
+
+    email: {
+        type: String,
+        unique: true,
+        lowercase:true,
+        trim:true,
+        required:true
+    },
+      studentid: {
+        type: Number,
+        require: true
+    },
+
+    department: {
+        type: String,
+        required: true,
+        enum:[btech,mca,mba,bca,bba],
+        lowercase:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['admin', 'student'],
+        default: 'student'
+    },
+
+    // isVoted: {
+    //     type: Boolean,
+    //     default: false
+    // },
+    // votedFor: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: 'candidateSch',
+    //     default: null
+    // }
+});
+
+//export
+//👉 Schemas define rules
+//👉Model interact with the database
+module.exports = mongoose.model('studentSch', studentSchema);
---
+++ Backend/models/voteSchema.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+
+const voteSchema=new mongoose.Schema({
+
+    //who vote
+    //whom votedd
+    //which election
+    //when voted
+})
